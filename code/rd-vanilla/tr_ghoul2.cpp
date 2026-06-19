ca0212b9569b9289282e5d3afc0ac2c4e8cad684
smbct <smbct@users.noreply.github.com>
2026-06-19 23:04:06 +0200

    setting the right axis order

--- code/rd-vanilla/tr_ghoul2.cpp
+++ code/rd-vanilla/tr_ghoul2.cpp
@@ -4571,8 +4571,8 @@ void drawSkeletons() {
 					// PITCH, YAW, ROLL
 					float humerus_angles_bis[3] = {humerus_angles[PITCH], humerus_angles[YAW], humerus_angles[ROLL]};
 
-					G2_Set_Bone_Angles(&ghoul2[model_ind], ghoul2[model_ind].mBlist, "rhumerus", humerus_angles_bis, BONE_ANGLES_REPLACE, first, second, third, 0, 0);
-					// G2_Set_Bone_Angles(&ghoul2[model_ind], ghoul2[model_ind].mBlist, "rhumerus", humerus_angles_bis, BONE_ANGLES_REPLACE, POSITIVE_X, POSITIVE_Y, POSITIVE_Z, 0, 0);
+					// 5 3 4
+					G2_Set_Bone_Angles(&ghoul2[model_ind], ghoul2[model_ind].mBlist, "rhumerus", humerus_angles_bis, BONE_ANGLES_REPLACE, NEGATIVE_Z, POSITIVE_Y, NEGATIVE_X, 0, 0);
 
 					
 
