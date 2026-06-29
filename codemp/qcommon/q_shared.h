28a0d464e6ab266ec4f79fb5521b785961c9ece2
smbct <smbct@users.noreply.github.com>
2026-06-29 23:57:55 +0200

    cleaning

--- codemp/qcommon/q_shared.h
+++ codemp/qcommon/q_shared.h
@@ -1430,9 +1430,6 @@ typedef struct usercmd_s {
 	byte			invensel;
 	byte			generic_cmd;
 	signed char	forwardmove, rightmove, upmove;
-
-	// kinect mod
-	
 } usercmd_t;
 
 //===================================================================
